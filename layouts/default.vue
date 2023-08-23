fa1c1397a17ff9517516ec51a471b0e3fc9c1c6a
Klim_Somov <Klim_somov@mail.ru>
2023-08-23 21:53:05 +0300

    реборн

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,7 @@
             </div>
             <div :class="{ 'modal-overlay': isMenu }"></div>
 
-            <ul :class="{ active: isMenu }" class="header_menus">
+            <ul :class="{ active: isMenu }" class="header_menus ">
               <li>
                 <NuxtLink @click="isMenu = false" to="/partners"
                   >Партнерам</NuxtLink
@@ -95,13 +95,26 @@
           </div>
           <div class="call_right">
             <div class="call_form">
-              <input type="text" placeholder="Имя" name="name" id="" />
-              <input type="tel" placeholder="Телефон" name="tel" id="" />
-              <button @click="handleSuccess" class="call_send">
+              <input
+                v-model="name"
+                type="text"
+                placeholder="Имя"
+                name="name"
+                id="" />
+              <input
+                v-maska
+                data-maska="+7##########"
+                v-model="phone"
+                type="tel"
+                placeholder="Телефон"
+                name="tel"
+                id="" />
+              <button @click="onSubmit" class="call_send">
                 Отправить заявку
               </button>
             </div>
             <Success v-if="succes" />
+            <Error v-if="error" />
           </div>
         </div>
         <div class="footer_block">
@@ -174,14 +187,48 @@
   </div>
 </template>
 <script setup>
+const { $csrfFetch } = useNuxtApp();
+
 const isMenu = ref(false);
 const succes = ref(false);
+const error = ref(false);
+const name = ref("");
+const phone = ref("");
 
-const handleSuccess = () => {
+const handleModalError = () => {
+  error.value = true;
+  setTimeout(() => {
+    error.value = false;
+  }, 3500);
+};
+
+const handleModalSubmit = () => {
   succes.value = true;
   setTimeout(() => {
     succes.value = false;
   }, 3500);
+};
+
+const onSubmit = () => {
+  if (!name.value || !phone.value) return;
+
+  $csrfFetch("/api/contact", {
+    method: "POST",
+    body: {
+      from: name.value,
+      phone: phone.value,
+      email: "",
+    },
+  })
+    .then(() => {
+      handleModalSubmit();
+    })
+    .catch((e) => {
+      handleModalError();
+      console.log(e);
+    });
+  name.value = "";
+  phone.value = "";
 };
 
 const menuToggle = () => {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ["@/assets/scss/main.scss"],
+  css: ["bootstrap/dist/css/bootstrap.min.css", "@/assets/scss/main.scss"],
   modules: ["nuxt-swiper", "nuxt-csurf"],
 
   app: {
     head: {
       title: "PayTaxi",
-      link: [
-        {
-          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
-          rel: "stylesheet",
-          integrity:
-            "sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65",
-          crossorigin: "anonymous",
-        },
-      ],
-      script: [
-        {
-          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js",
-        },
-      ],
     },
   },
 
